f3e78e4933c396929106ed568514e8b446053423
f55552b <zhewen555@gmail.com>
2025-04-13 04:44:47 +0800

    added some func for enquiry, edited main

--- src/bto/Main.java
+++ src/bto/Main.java
@@ -1,19 +1,26 @@
 package bto;
 
 import java.util.Scanner;
+import java.util.ArrayList;
+
+import bto.model.Enquiry;
+import bto.model.UserPerson;
 
 public class Main {
     public static void main(String[] args) {
-        User[] Users; // creating a array to store all users
+    	//UserPerson[] Users = new UserPerson[20]; // creating a array to store all users
+    	
+    	ArrayList<UserPerson> Users = new ArrayList<>();
+    	
         Applicant.LoadUsers(Users); // need a method in applicant to edit an array(as argument) and load in all user from applicant list
         HDBofficer.LoadUsers(Users);
         HDBmanager.LoadUsers(Users);
         
-        int logout;
         //login menu
+        int logout;
         do {
         Scanner sc= new Scanner(System.in);
-        User cur= null; // current user, need a pointer
+        UserPerson cur= null; // current user, need a pointer
         do {
 	        System.out.println("BTO System login page \n");
 	        System.out.println("Pls input username:");
@@ -23,9 +30,11 @@ public class Main {
 	        String password = sc.nextLine();
 	        
 	        
-	        for (User user : Users) {
-	        	if(username.equals(user.NRIC)) {
-	        		if(password.equals(user.password)) {
+	        for (UserPerson user : Users) {
+	        	//if (user == null) continue;
+	        	
+	        	if(username.equals(user.getNRIC())) {
+	        		if(password.equals(user.getPassword())) {
 	        			cur= user;
 	        			break;
 	        		}
@@ -39,41 +48,71 @@ public class Main {
         }while(cur== null);
         
         //first interface 1 menu for each user type
-        System.out.println("Welcome" + cur.userType + " " + cur.name +"\n");
+        System.out.println("Welcome" + " <" + cur.getUserType() + "> " + cur.getName() +"!\n");
+        
+        //if cur pointer cannot just init 3 cur for diff obj
         
         int selection;
-        if(cur.userType == "manager") { //if possible change to check the obj itself
-	        System.out.println("Pls input seletion:");
-	        System.out.println("Pls input PW:");
-	        System.out.println("Logout");
-        }else if(cur.userType == "officer") {
-        	System.out.println("Logout");
-        }else if(cur.userType == "applicant"){// for applicant
+        if(cur.getUserType() == "manager") { //if possible change to check the obj itself
+        	System.out.println("1. View All BTO projects"); 
+        	System.out.println("2. Toggle visibility");//if not officer in charge
+        	System.out.println("3. approve BTO application by applicant");//both withdraw and apply
+        	System.out.println("4. approve officer application by officer");// put view function within
+        	System.out.println("5. Add BTO listing");
+        	System.out.println("6. Edit BTO listing");
+        	System.out.println("7. Delete BTO listing");
+        	System.out.println("8. Logout");
+        	System.out.println("9. change PW");
+        	System.out.println("10. filer by area/flat type availability/px/date");
+        	System.out.println("11. reply enquries");
+        	System.out.println("12. generate report");
+        	System.out.println("Pls input seletion: (e.g. 5)");
+        }else if(cur.getUserType() == "officer") {// no need the enquiries list attribute and applied projID attribute cause applicant class alr have
+        	System.out.println("1. View BTO project/or only the project you applied for(if applicable)"); 
+        	System.out.println("2. Apply for BTO project");//if not officer in charge
+        	System.out.println("3. Withdraw BTO project");
+        	System.out.println("4. View enquries");
+        	System.out.println("5. Add enquries");
+        	System.out.println("6. Edit enquries");
+        	System.out.println("7. Delete enquries");
+        	System.out.println("8. Logout");
+        	System.out.println("9. change PW");
+        	System.out.println("10. filer by area/flat type availability/px/date");
+        	System.out.println("--------officer functions----------");
+        	System.out.println("11. reply enquries");
+        	System.out.println("12. apply for officer of a project");
+        	//System.out.println("12. approve BTO application by applicant"); wait approve by manage is it?
+        	System.out.println("13. book slot for applicant on approved applications");
+        	System.out.println("14. generate receipt");
+        	System.out.println("Pls input seletion: (e.g. 5)");
+        	
+        }else if(cur.getUserType() == "applicant"){// for applicant
         	do {
 	        	System.out.println("List of actions:");
 	        	System.out.println("1. View BTO project/or only the project you applied for(if applicable)");//not sure if this still have after they applied/approved for one, 1 for 1 
 	        	System.out.println("2. Apply for BTO project");
 	        	System.out.println("3. Withdraw BTO project");
 	        	System.out.println("4. View enquries");
-	        	System.out.println("5. Add enquries");//not sure if add here or under project
+	        	System.out.println("5. Add enquries");//not sure if add here or under project, add here as 1 bulk for whole program
 	        	System.out.println("6. Edit enquries");//should be an optional option? cause need check if have existing
 	        	System.out.println("7. Delete enquries");
 	        	System.out.println("8. Logout");
 	        	System.out.println("9. change PW");
+	        	System.out.println("10. filer by area/flat type availability/px/date");
 	        	System.out.println("Pls input seletion: (e.g. 5)");
 	        	selection = sc.nextInt();
 	        	switch (selection) {
 	            case 1://display the project they applied for only if they applied
 	                System.out.println("Case 1 selected.");
-	                if(cur.applicationStatus!= "None") {// maybe we no need withdrawn and unsuccessful state?
-	                	Project.display(cur.appliedProjectId);//add a display method in project to display a single project
-	                }else if(cur.applicationStatus== "None") {
+	                if(cur.getApplicationStatus()!= "None") {// maybe we no need withdrawn and unsuccessful state?
+	                	Project.display(cur.getAppliedProjectId());//add a display method in project to display a single project
+	                }else if(cur.getApplicationStatus()== "None") {
 	                	Project.displayAppProjects();// need check visibility rmb sm can change visibility add a display app method 
 	                }
 	                break;
 	            case 2:
 	            	int projectID, rtsel;
-	            	string rt;
+	            	String rt;
 	                System.out.println("Case 2 selected.");
 	                System.out.println("enter project ID of desired project: (e.g. 1)");
 	                projectID=sc.nextInt();
@@ -102,37 +141,78 @@ public class Main {
 	                		System.out.println("Unsuccessful!, likely having ongoing application!");
 	                	}
 	                }else {
-	                	System.out.println("not elligible!");
+	                	System.out.println("not eligible!");
 	                }
 	                break;
 	            case 3:
 	                System.out.println("Case 3 selected.");
-	                if(cur.applicationStatus!= "None") {// not sure need add unsuccessful
+	                if(cur.getApplicationStatus()!= "None") {// not sure need add unsuccessful
 	                	cur.withdrawApplication();//need to send to officer 
 	                	System.out.println("Successful!, sent to officer for approval");
-	                }else if(cur.applicationStatus== "None"){
+	                }else if(cur.getApplicationStatus()== "None"){
 	                	System.out.println("Unsuccessful!, no current ongoing application");
 	                }
 	                break;
 	            case 4:
-	                System.out.println("Case 4 selected.");
-	                
+	                System.out.println("Case 4 selected.");//delete list of enquries attribute under applicant
+	                Enquiry.displayAllEnquiries();
 	                break;
 	            case 5:
 	                System.out.println("Case 5 selected.");
+	                System.out.println("Input message:");
+	                Enquiry.addEnquiry(Enquiry(Enquiry.getCount(), cur.getName(), sc.nextLine()));// output success msg alr
 	                break;
 	            case 6:
+	            	int enquiryID;
 	                System.out.println("Case 6 selected.");
+	                //optimise by creating a new array to store the user exclusive enquiries
+	                if(Enquiry.displayByUser(cur.getName())) {
+	                	System.out.println("input enquiry ID to select enquiry to edit:");
+	                	enquiryID=sc.nextInt();
+	                	if(Enquiry.getByID(enquiryID)==null) {
+	                		System.out.println("invalid ID");
+	                	}else if(Enquiry.getByID(enquiryID).getApplicantName()!=cur.getName()) {
+	                		System.out.println("invalid access, not under your name");
+	                	}else {//both got an enquiry and under user's name
+	                		System.out.println("current enqury:");
+	                		Enquiry.getByID(enquiryID).display();
+	                		System.out.println("input new msg:");
+	                		Enquiry.editEnquiry(enquiryID, sc.nextLine());//success msg included
+	                	}
+	                }
 	                break;
 	            case 7:
+	            	int enquiryID1;
 	                System.out.println("Case 7 selected.");
+	                if(Enquiry.displayByUser(cur.getName())) {
+	                	System.out.println("input enquiry ID to select enquiry to delete:");
+	                	enquiryID1=sc.nextInt();
+	                	if(Enquiry.getByID(enquiryID1)==null) {
+	                		System.out.println("invalid ID");
+	                	}else if(Enquiry.getByID(enquiryID1).getApplicantName()!=cur.getName()) {
+	                		System.out.println("invalid access, not under your name");
+	                	}else {//both got an enquiry and under user's name
+	                		System.out.println("current enqury:");
+	                		Enquiry.getByID(enquiryID1).display();
+	                		Enquiry.deleteEnquiry(enquiryID1);//success msg included
+	                	}
+	                }
 	                break;
 	            case 8:
 	                System.out.println("Case 8 selected.");
 	                System.out.println("logging out");
 	                break;
 	            case 9:
+	            	String pw;
 	                System.out.println("Case 9 selected.");
+	                System.out.println("input new pw:");
+	                pw=sc.nextLine();
+	                System.out.println("confirm new pw:(input same pw agn)");
+	                if(pw==sc.nextLine()) {
+	                	cur.changePassword(pw);
+	                }else {
+	                	System.out.println("Unsuccessful change, re-enter pw not same");
+	                }
 	                break;
 	            default:
 	                System.out.println("Invalid choice. Try again");
--- src/bto/model/Enquiry.java
+++ src/bto/model/Enquiry.java
@@ -1,22 +1,115 @@
 package bto.model;
 
+import bto.Enquiry;
+
 public class Enquiry {
     private int enquiryID;
     private String applicantName;
     private String message;
-    private String response;
-    private boolean status;
-    private int projectId;
+    private String response;// sld be a list?
+    private boolean status;//true means open for reply?
+    //private int projectId;//still need? since enquiry not by project anymore
+    
+    private static Enquiry[] Enquiries = new Enquiry[100];
+    private static int count = 0;
 
-    public Enquiry(int enquiryID, String applicantName, String message, int projectId) {
+    public Enquiry(int enquiryID, String applicantName, String message) {
         this.enquiryID = enquiryID;
         this.applicantName = applicantName;
         this.message = message;
         this.response = "";
         this.status = false;
-        this.projectId = projectId;
+        //this.projectId = projectId;
+    }
+    
+    public void display() {
+        System.out.println("Enquiry ID: " + enquiryID);
+        System.out.println("Applicant Name: " + applicantName);
+        System.out.println("Message: " + message);
+        System.out.println("Response: " + (response.isEmpty() ? "No response yet." : response));
+        System.out.println("Status: " + (status ? "Open" : "Closed"));
+        //System.out.println("Project ID: " + projectId);
+        System.out.println("-------------------------------");
+    }
+    
+    public static boolean displayByUser(String applicantName) {
+        boolean found = false;
+
+        for (int i = 0; i < count; i++) {
+            if (Enquiries[i].getApplicantName().equals(applicantName)) {
+                Enquiries[i].display();
+                found = true;
+            }
+        }
+
+        if (!found) {
+            System.out.println("No enquiries found for user: " + applicantName);
+        }
+
+        return found;
+    }
+    
+    public static Enquiry getByID(int enquiryID) {
+        for (int i = 0; i < count; i++) {
+            if (Enquiries[i].getEnquiryID() == enquiryID) {
+                return Enquiries[i];
+            }
+        }
+        return null; // not found
     }
 
+    // Display all enquiries
+    public static void displayAllEnquiries() {
+        if (count == 0) {
+            System.out.println("No enquiries found.");
+            return;
+        }
+
+        for (int i = 0; i < count; i++) {
+            Enquiries[i].display();
+        }
+    }
+
+    // Add enquiry
+    public static void addEnquiry(Enquiry e) {
+        if (count >= Enquiries.length) {
+            System.out.println("Enquiry list full.");
+            return;
+        }
+        Enquiries[count] = e;
+        count++;
+        System.out.println("Enquiry added successfully.");
+    }
+
+    // Edit enquiry
+    public static void editEnquiry(int enquiryID, String newMessage) {
+        for (int i = 0; i < count; i++) {
+            if (Enquiries[i].getEnquiryID() == enquiryID) {
+                Enquiries[i].setMessage(newMessage);
+                System.out.println("Enquiry " + enquiryID + " updated.");
+                return;
+            }
+        }
+        System.out.println("Enquiry not found.");
+    }
+
+    // Delete enquiry
+    public static void deleteEnquiry(int enquiryID) {
+        for (int i = 0; i < count; i++) {
+            if (Enquiries[i].getEnquiryID() == enquiryID) {
+                for (int j = i; j < count - 1; j++) {
+                    Enquiries[j] = Enquiries[j + 1];
+                }
+                Enquiries[count - 1] = null;
+                count--;
+                System.out.println("Enquiry " + enquiryID + " deleted.");
+                return;
+            }
+        }
+        System.out.println("Enquiry not found.");
+    }
+    
+    
     public void reply(String replyMessage) {
         this.response = replyMessage;
         System.out.println("Reply Sent: " + replyMessage);
@@ -41,9 +134,9 @@ public class Enquiry {
         return applicantName;
     }
     
-    public int getProjectId() {
+    /*public int getProjectId() {
         return projectId;
-    }
+    }*/
 
     public void setApplicantName(String applicantName) {
         this.applicantName = applicantName;
@@ -57,6 +150,9 @@ public class Enquiry {
     public void setMessage(String message) {
         this.message = message;
     }
-
+    
+    public static int getCount() {
+        return count;
+    }
 }
 
