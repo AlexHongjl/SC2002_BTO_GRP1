31869f5df8a7a85c1fb5afd8d946a450eb889e3d
whupdido <88324453+whupdido@users.noreply.github.com>
2025-04-12 22:19:01 +0800

    updated hdbmanager,officerregistration and project

--- src/bto/Project.java
+++ src/bto/Project.java
@@ -2,6 +2,7 @@ package bto;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Project {
     private int projectId;
@@ -10,55 +11,72 @@ public class Project {
     private int twoRoomCount;
     private int threeRoomCount;
     private boolean projectVisibility;
-    private int managerInCharge;
+    private HDBmanager managerInCharge;
     private int officerSlots;
     private LocalDate openDate;
     private LocalDate closeDate;
-    private List<Integer> officerlist;
-    private List<Integer> officerApplicantList;
+    private List<HDBofficer> officerList;
+    private List<OfficerRegistration> officerApplicantList;
     
     private static Project[] projects = new Project[100];
     private static int count = 0;
     
     
 
-    public Project(int projectId, String projectName, String manager) {
-        this.projectId = projectId;
-        this.projectName = projectName;
-        this.managerInCharge = manager;
-        this.officerApplicantList = new ArrayList<>();
+    public Project(String projectName, String neighbourhood,
+            int twoRoomCount, int threeRoomCount,
+            boolean projectVisibility, LocalDate openDate,
+            LocalDate closeDate, int officerSlots, HDBmanager managerInCharge) {
+
+    	this.projectId = setCount(getCount() + 1); // Unique project ID
+    	this.projectName = projectName;
+    	this.neighbourhood = neighbourhood;
+    	this.twoRoomCount = twoRoomCount;
+    	this.threeRoomCount = threeRoomCount;
+    	this.projectVisibility = projectVisibility;
+    	this.openDate = openDate;
+    	this.closeDate = closeDate;
+    	this.officerSlots = officerSlots;
+    	this.managerInCharge = managerInCharge;
+    	this.officerList = new ArrayList<>();
+    	this.officerApplicantList = new ArrayList<>();
+
+    	if (getCount() <= projects.length) {
+    		projects[getCount() - 1] = this; // Add to project list
+    	} else {
+    		System.out.println("Error: Maximum number of projects reached.");
+    	}
     }
+
 
     public void displayOfficerList() {
         System.out.println("Displaying Officer List...");
-    }
-
-    public void displayApplicantList() {
-        System.out.println("Displaying Applicant List...");
-    }
-    
-    public int getTwoRoomAvailability() {
-    	return twoRoomCount-officerApplicantList.size();
-    }
-    
-  //------------------------------------------------------------------separate proj list functions
-    
-    public static void addProject(Project project) {
-        if (count < projects.length) {
-            projects[count++] = project;
-        } else {
-            System.out.println("Project array is full.");
+        for (HDBofficer officer : officerList) {
+            System.out.println("Officer: " + officer.getName() + " (NRIC: " + officer.getNRIC() + ")");
         }
     }
 
+    public void displayApplicantList() {
+        System.out.println("Displaying Officer Applicant List...");
+        for (OfficerRegistration reg : officerApplicantList) {
+            System.out.println("Applicant: " + reg.getOfficer().getName() + 
+                               " (NRIC: " + reg.getOfficer().getNRIC() + 
+                               ") - Status: " + reg.getRegistrationStatus());
+        }
+    }
+    
+    
+  //------------------------------------------------------------------separate proj list functions
+    
+
     public static boolean removeProjectById(int projectId) {
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < getCount(); i++) {
             if (projects[i].projectId == projectId) {
-                for (int j = i; j < count - 1; j++) {
+                for (int j = i; j < getCount() - 1; j++) {
                     projects[j] = projects[j + 1];
                 }
-                projects[count - 1] = null;
-                count--;
+                projects[getCount() - 1] = null;
+                setCount(getCount() - 1);
                 return true;
             }
         }
@@ -66,7 +84,7 @@ public class Project {
     }
 
     public static Project getProjectById(int projectId) {
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < getCount(); i++) {
             if (projects[i].projectId == projectId) {
                 return projects[i];
             }
@@ -75,10 +93,19 @@ public class Project {
     }
 
     public static void displayAllProjects() {
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < getCount(); i++) {
             System.out.println("Project ID: " + projects[i].projectId + ", Name: " + projects[i].projectName);
         }
     }
+    public void addOfficerRegistration(OfficerRegistration registration) {
+        officerApplicantList.add(registration);
+    }
+    public void addOfficer(HDBofficer officer) {
+        if (!officerList.contains(officer)) {
+            officerList.add(officer);
+        }
+    }
+    
     
     //------------------------------------------------------------------separate get set functions
     
@@ -130,12 +157,59 @@ public class Project {
         this.projectVisibility = projectVisibility;
     }
 
-    public int getManagerInCharge() {
+    public HDBmanager getManagerInCharge() {
         return managerInCharge;
     }
 
-    public void setManagerInCharge(int managerInCharge) {
-        this.managerInCharge = managerInCharge;
+
+    public List<OfficerRegistration> getOfficerRegistrations() {
+        return officerApplicantList;
     }
+    
+
+    public List<HDBofficer> getOfficers() {
+        return officerList;
+    }
+    
+
+    public int getProjectID() {
+        return projectId;
+    }
+    public LocalDate getOpeningDate() {
+        return openDate;
+    }
+    
+    public void setOpeningDate(LocalDate openDate) {
+        this.openDate = openDate;
+    }
+    
+    public LocalDate getClosingDate() {
+        return closeDate;
+    }
+    
+    public void setClosingDate(LocalDate closeDate) {
+        this.closeDate = closeDate;
+    }
+    public int getOfficerSlots() {
+        return officerSlots;
+    }
+    
+    public void setOfficerSlots(int officerSlots) {
+        this.officerSlots = officerSlots;
+    }
+    public static Project[] getProjects() {
+    	return projects;
+    }
+
+
+	public static int getCount() {
+		return count;
+	}
+
+
+	public static int setCount(int count) {
+		Project.count = count;
+		return count;
+	}
 }
 
